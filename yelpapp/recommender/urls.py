dac0bf812e17ca0c2f6dad46fd2cbbe079821ab0
Juan Jose Villegas <juan.villegas@rappi.com>
2020-04-09 09:19:04 -0500

    Added yelpapp sckeleton

---
+++ yelpapp/recommender/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path('', views.HomeView.as_view(), name='index'),
+    # path('recomendations/<user_id>/', views.user_recommendations, name='user_recommendations'),
+    # path('item_neighbors/<artist_name>', views.item_neighbors, name='item_neighbors')
+]
