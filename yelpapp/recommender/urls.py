00bb29dbae491ac074f525bbf4cb18ea69fe0d1c
Juan Sebastian Mendez <juansebastian.mendezr@gmail.com>
2020-04-27 20:21:50 -0500

    AddBusiness and AddReview functions were included in yelpapp

--- yelpapp/recommender/urls.py
+++ yelpapp/recommender/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.HomeView.as_view(), name='index'),
     path('reviews/<business_id>/', views.business_reviews, name='business_reviews'),
     path('neighbours/<business_id>/', views.business_neighbours, name='business_neighbours'),
+    path('addBusiness', views.add_business),
+    path('addReview', views.add_review),
 ]
--- yelpapp/recommender/views.py
+++ yelpapp/recommender/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.core.paginator import Paginator
-from django.http import Http404
+from django.http import Http404, JsonResponse
 from mongoengine import connect
+import uuid
 
 from .models import Business, Review
 connect('yelp')
@@ -27,6 +28,7 @@ def business_reviews(request, business_id):
     if(business):
         reviews = Review.objects(business_id=business_id)
         context = {
+            'business_id': business.business_id,
             'business_name': business.name,
             'reviews': reviews
         }
@@ -37,3 +39,17 @@ def business_reviews(request, business_id):
 def business_neighbours(request, business_id):
     context = {}
     return render(request, 'business_neighbours.html', context=context)
+
+def add_business(request):
+    new_business = Business(business_id=str(uuid.uuid4()),name=request.GET['name'], address=request.GET['address'], city=request.GET['city'], state=request.GET['state'], postal_code=request.GET['postalcode'], categories=request.GET['categories'], review_count=0, stars=0)
+    new_business.save()
+
+    data = { 'allright': True }
+    return JsonResponse(data)
+
+def add_review(request):
+    new_review = Review(review_id=str(uuid.uuid4()), business_id=request.GET['business_id'], stars=float(request.GET['stars']), text=request.GET['text'])
+    new_review.save()
+
+    data = { 'allright': True }
+    return JsonResponse(data)
